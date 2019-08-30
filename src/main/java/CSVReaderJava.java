96401c5eea6630d8a55a6e515e6ab0007803dff5
SrivPrat27 <srivastavapratyush27@yahoo.in>
2019-08-30 17:44:42 +0530

    Using file written by FileGenerator to write into objects

--- src/main/java/CSVReaderJava.java
+++ src/main/java/CSVReaderJava.java
@@ -5,17 +5,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 class CSVReaderJava {
-    public List<Book> initialize()throws IOException {
-        FileReader fileReader = new FileReader("/home/pratyush/Testing/File.csv");
+    public List<Book> initialize() throws IOException {
+
+        RandomFileGenerator randomFileGenerator = new RandomFileGenerator();
+        randomFileGenerator.createCsvFileWithRandomValues();
+
+        FileReader fileReader = new FileReader("/home/pratyush/Testing/Write.csv");
         BufferedReader bufferedReader = new BufferedReader(fileReader);
+
         String line = null;
         List<Book> listOfBooks = new ArrayList<Book>();
 
         while ((line = bufferedReader.readLine()) != null) {
+
             String temp[] = line.split(",");
-            System.out.println(temp.length);
-            listOfBooks.add(new Book(temp[0],temp[1],temp[2],temp[3],temp[4],Integer.parseInt(temp[5]),Double.parseDouble(temp[6]),BindingType.valueOf(temp[7])));
+            listOfBooks.add(new Book(temp[0], temp[1], temp[2], temp[3], temp[4], Integer.parseInt(temp[5]), Double.parseDouble(temp[6]), BindingType.valueOf(temp[7])));
+
         }
+
         return listOfBooks;
     }
+
 }
+
